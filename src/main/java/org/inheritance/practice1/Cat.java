2e55a9b981ce73e43ef95cfc85dd2629899eff4a
Anshuvats <kumaranshu16101999@gmail.com>
2025-03-08 11:44:33 +0530

    Initiate with Inheritance

---
+++ src/main/java/org/inheritance/practice1/Cat.java
@@ -0,0 +1,9 @@
+package org.inheritance.practice1;
+
+public class Cat extends Animal {
+
+    @Override
+    public void makeSound(){
+        System.out.println("Cat's Sound is meuooo");
+    }
+}
---
+++ src/main/java/org/inheritance/practice1/Main.java
@@ -0,0 +1,10 @@
+package org.inheritance.practice1;
+
+public class Main {
+    public static void main(String[] args){
+        Animal animal=new Animal();
+        animal.makeSound();
+        Cat cat=new Cat();
+        cat.makeSound();
+    }
+}
